9dd0837ee6e429d76e5c5fc2ba4a230e956412db
miloszwasacz <wasacz.dev@gmail.com>
2024-05-29 01:38:11 +0100

    Refactor `IfASTNode` to support `else if`s

--- src/ast/expr/if.rs
+++ src/ast/expr/if.rs
@@ -2,6 +2,8 @@
 
 use std::{fmt, iter};
 
+use debug_tree::TreeBuilder;
+
 use crate::ast::{
     ast_defaults, ASTChildIterator, ASTNode, AsASTNode, AssigneeExprASTNode, BlockASTNode,
     ExprASTNode, PlaceExprASTNode, ValueExprASTNode,
@@ -14,37 +16,102 @@ pub struct IfASTNode {
     /// The condition can be [any kind of expression](ExprASTNode).
     condition: Box<dyn ExprASTNode>,
     then_block: Box<BlockASTNode>,
-    else_block: Option<Box<BlockASTNode>>,
+    else_node: ElseExpr,
     span: Span,
 }
 
+/// An enum representing the possible else expressions in an if statement.
+#[derive(Debug)]
+pub enum ElseExpr {
+    /// No else block.
+    None,
+    /// An else block.
+    Else(Box<BlockASTNode>),
+    /// An else if block.
+    ElseIf(Box<IfASTNode>),
+}
+
 impl IfASTNode {
-    /// Creates a new `IfASTNode` with the given condition, then block, else block and span.
+    /// Creates a new `IfASTNode` with the given condition, then block, else expression and span.
     pub fn new(
         condition: Box<dyn ExprASTNode>,
         then_block: Box<BlockASTNode>,
-        else_block: Box<BlockASTNode>,
+        else_node: ElseExpr,
         span: Span,
     ) -> IfASTNode {
         IfASTNode {
             condition,
             then_block,
-            else_block: Some(else_block),
+            else_node,
             span,
         }
     }
 
-    /// Creates a new `IfASTNode` with the given condition, then block and span.
-    pub fn new_without_else(
-        condition: Box<dyn ExprASTNode>,
-        then_block: Box<BlockASTNode>,
-        span: Span,
-    ) -> IfASTNode {
-        IfASTNode {
-            condition,
-            then_block,
-            else_block: None,
-            span,
+    /// Collects all the else if nodes and the final else block (if any) in a flat vector.
+    fn flatten_else_ifs(&self) -> (Vec<&IfASTNode>, Option<&BlockASTNode>) {
+        let mut nodes = Vec::new();
+        let mut current = self;
+        while let ElseExpr::ElseIf(if_node) = &current.else_node {
+            nodes.push(if_node.as_ref());
+            current = if_node;
+        }
+        let else_block = match &current.else_node {
+            ElseExpr::None => None,
+            ElseExpr::Else(block) => Some(block.as_ref()),
+            ElseExpr::ElseIf(_) => unreachable!(),
+        };
+
+        (nodes, else_block)
+    }
+
+    /// Custom implementation for [`ASTNode::add_to_tree_string`] that properly
+    /// handles the else if case.
+    ///
+    /// In the case of an else if, the tree is flattened so that all the else ifs
+    /// and the final else (if any) are at the same level.
+    fn add_to_tree_string(&self, builder: &mut TreeBuilder, is_else_if: bool) {
+        // We add a new branch only if this is the main if node, not a nested else if
+        let branch = if !is_else_if {
+            Some(builder.add_branch(format!("{self}").as_str()))
+        } else {
+            None
+        };
+
+        // Condition
+        {
+            let condition = self.condition.as_ast();
+            let mut branch = builder.add_branch("Condition");
+            condition.add_to_tree_string(builder);
+            branch.release();
+        }
+
+        // Then block
+        {
+            let then_block = self.then_block.as_ast();
+            let mut branch = builder.add_branch("Then");
+            then_block.add_to_tree_string(builder);
+            branch.release();
+        }
+
+        // The branch only exists if this is the main if node, not a nested else if
+        if let Some(mut branch) = branch {
+            let (else_ifs, else_block) = self.flatten_else_ifs();
+
+            // Else if blocks
+            for else_if in else_ifs {
+                let mut branch = builder.add_branch(format!("Else {else_if}").as_str());
+                IfASTNode::add_to_tree_string(else_if, builder, true);
+                branch.release();
+            }
+
+            // Else block
+            if let Some(else_block) = else_block {
+                let mut branch = builder.add_branch("Else");
+                else_block.add_to_tree_string(builder);
+                branch.release();
+            }
+
+            branch.release();
         }
     }
 }
@@ -55,9 +122,24 @@ impl ASTNode for IfASTNode {
     fn children(&self) -> Option<ASTChildIterator> {
         let condition = iter::once(self.condition.as_ast());
         let then_block = iter::once(self.then_block.as_ast());
-        let else_block = self.else_block.iter().map(|b| b.as_ast());
-        let iter = condition.chain(then_block).chain(else_block);
-        Some(Box::new(iter))
+        let iter = condition.chain(then_block);
+        Some(match &self.else_node {
+            ElseExpr::None => Box::new(iter),
+            ElseExpr::Else(block) => {
+                let else_block = iter::once(block.as_ast());
+                let iter = iter.chain(else_block);
+                Box::new(iter)
+            }
+            ElseExpr::ElseIf(if_node) => {
+                let else_if = iter::once(if_node.as_ast());
+                let iter = iter.chain(else_if);
+                Box::new(iter)
+            }
+        })
+    }
+
+    fn add_to_tree_string(&self, builder: &mut TreeBuilder) {
+        IfASTNode::add_to_tree_string(self, builder, false);
     }
 }
 
